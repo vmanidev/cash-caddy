9a182c9e8cd76ef11f8d5a1930cd66f5f72fd15d
vmanidev <vmanidev@outlook.com>
2025-10-15 00:32:39 +0530

    feat(ui): add styled transaction summary cards to Overview section

--- src/features/overview/Overview.tsx
+++ src/features/overview/Overview.tsx
@@ -1,32 +1,36 @@
-import { Grid, Paper } from "@mui/material";
+import { Divider, Grid, Paper, Stack } from "@mui/material";
 
 import styles from "./Overview.module.scss";
 
 function Overview() {
+  const TransactionSummary = () =>
+    [
+      { type: "income", displayText: "Total Income", value: "23000" },
+      { type: "expenses", displayText: "Total Expenses", value: "3000" },
+      { type: "balance", displayText: "Total Balance", value: "20000" },
+    ].map(({ type, displayText, value }) => (
+      <Grid size={4} key={type}>
+        <Paper className={`${styles.paper} ${styles[type]}`} elevation={4}>
+          <Stack
+            direction="column"
+            divider={<Divider orientation="horizontal" flexItem />}
+            spacing={2}
+            justifyContent="center"
+            alignItems="center"
+          >
+            <div>{displayText}</div>
+            <div className={styles.valueHolder}>₹ {value}</div>
+          </Stack>
+        </Paper>
+      </Grid>
+    ));
+
   return (
-    <Grid container spacing={6}>
-      <Grid size={4}>
-        <Paper
-          className={`${styles.paper} ${styles.incomePaper}`}
-          elevation={4}
-        >
-          Total Income
-        </Paper>
+    <>
+      <Grid container spacing={6}>
+        <TransactionSummary />
       </Grid>
-      <Grid size={4}>
-        <Paper
-          className={`${styles.paper} ${styles.expensesPaper}`}
-          elevation={4}
-        >
-          Total Expenses
-        </Paper>
-      </Grid>
-      <Grid size={4}>
-        <Paper className={styles.paper} elevation={4}>
-          Total Balance
-        </Paper>
-      </Grid>
-    </Grid>
+    </>
   );
 }
 
