69a3a4e2c9b2b8915e7e26648fe4aaf7991e1923
vmanidev <vmanidev@outlook.com>
2025-10-20 03:43:45 +0530

    feat(ui): add edit transaction feature

--- src/components/common/transactions/TransactionRows.tsx
+++ src/components/common/transactions/TransactionRows.tsx
@@ -2,42 +2,82 @@ import { Delete, Edit } from "@mui/icons-material";
 import { Button, ButtonGroup, TableCell, TableRow } from "@mui/material";
 import { useSelector } from "react-redux";
 import type { Transaction } from "../../../store/types";
+import { useState } from "react";
+import UpdateTransaction from "./UpdateTransaction";
+import { initialData } from "../../../constants/form";
+import type { UpdateTransactionStateProps } from "../../../models/transactions";
+import type { FormData } from "../../../models/form";
 
 interface Props {
   count?: number;
 }
 
 function TransactionRows({ count }: Props) {
+  const [editTransaction, setEditTransaction] =
+    useState<UpdateTransactionStateProps>({
+      formData: initialData,
+      showModal: false,
+    });
+
   const transactionData: Transaction[] = useSelector(
     (state: any) => state.transactions
   );
 
-  return transactionData
-    .slice(0, count)
-    .map(({ id, date, amount, category, type, note }) => {
-      return (
-        <TableRow key={id} hover>
-          <TableCell>{date}</TableCell>
-          <TableCell>{note}</TableCell>
-          <TableCell>{category}</TableCell>
-          <TableCell
-            className={type === "income" ? "income-text" : "expenses-text"}
-          >
-            {amount}
-          </TableCell>
-          <TableCell>
-            <ButtonGroup variant="text">
-              <Button>
-                <Edit />
-              </Button>
-              <Button>
-                <Delete color="error" />
-              </Button>
-            </ButtonGroup>
-          </TableCell>
-        </TableRow>
-      );
-    });
+  const getTransactionRows = () => {
+    return transactionData
+      .slice(0, count)
+      .map(({ id, date, amount, category, type, note }) => {
+        return (
+          <TableRow key={id} hover>
+            <TableCell>{date}</TableCell>
+            <TableCell>{note}</TableCell>
+            <TableCell>{category}</TableCell>
+            <TableCell
+              className={type === "income" ? "income-text" : "expenses-text"}
+            >
+              {amount}
+            </TableCell>
+            <TableCell>
+              <ButtonGroup variant="text">
+                <Button
+                  onClick={() =>
+                    editTransactionRow({
+                      id,
+                      date,
+                      amount,
+                      category,
+                      type,
+                      note,
+                    })
+                  }
+                >
+                  <Edit />
+                </Button>
+                <Button>
+                  <Delete color="error" />
+                </Button>
+              </ButtonGroup>
+            </TableCell>
+          </TableRow>
+        );
+      });
+  };
+
+  const editTransactionRow = (formData: FormData) => {
+    setEditTransaction((prev: any) => ({ ...prev, formData, showModal: true }));
+  };
+
+  return (
+    <>
+      {editTransaction.showModal && (
+        <UpdateTransaction
+          updateTransaction={editTransaction}
+          setUpdateTransaction={setEditTransaction}
+        />
+      )}
+      {getTransactionRows()}
+    </>
+  );
 }
 
 export default TransactionRows;
