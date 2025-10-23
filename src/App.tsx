395ec239f775cae83d63355acbbf64aa6d808223
vmanidev <vmanidev@outlook.com>
2025-10-24 01:43:28 +0530

    feat(storage): sync transactions in localStorage using custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { useSelector } from "react-redux";
 import "./App.scss";
 import AppRouter from "./routes/AppRouter";
+import useLocalStorageSync from "./hooks/useLocalStorageSync";
 
 function App() {
+  //Sync transactions in localStorage
+  const transactions = useSelector((store: any) => store.transactions);
+  useLocalStorageSync({ transactions });
+
   return <AppRouter />;
 }
 
