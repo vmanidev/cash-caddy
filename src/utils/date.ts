8cf6f63d48d7b77430519c36380d96f60580a210
vmanidev <vmanidev@outlook.com>
2025-10-25 00:48:10 +0530

    feat(ui): add sortByDate helper in date util

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,8 +1,13 @@
+import type { Transaction } from "../store/types";
+
 function formatDate(date: string) {
 
     const getLocaleDate = () => Intl.DateTimeFormat("en-us", { dateStyle: "medium" }).format(new Date(date));
 
     return { getLocaleDate }
 }
+
+export const sortByDate = (transactions: Transaction[]) =>
+    transactions.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
 
 export default formatDate;
