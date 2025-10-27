776f35855f67b9c8dbe8b46c90466e97687bfac7
vmanidev <vmanidev@outlook.com>
2025-10-28 00:03:21 +0530

    feat(ui): add boilerplate and routing for category page

---
+++ src/pages/categories/Categories.tsx
@@ -0,0 +1,14 @@
+import { Grid } from "@mui/material";
+import AppHeader from "../../components/common/header/Header";
+
+function Categories() {
+  return (
+    <Grid container spacing={2} margin={2} size={12}>
+      <Grid size={12}>
+        <AppHeader />
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Categories;
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "../pages/dashboard/Dashboard";
 import Transactions from "../pages/transactions/Transactions";
+import Categories from "../pages/categories/Categories";
 
 function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/transactions" element={<Transactions />} />
+      <Route path="/categories" element={<Categories />} />
     </Routes>
   );
 }
