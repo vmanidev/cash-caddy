45f120acdc36eb9388b755a4205b891a656f62d9
vmanidev <vmanidev@outlook.com>
2025-10-27 23:14:26 +0530

    feat(ui): add icons and type-safe menu items in sidebar

---
+++ src/constants/sidebar.ts
@@ -0,0 +1,23 @@
+import type { MenuItems } from "../models/sidebar";
+
+export const menuItems: MenuItems[] = [{
+    key: "dashboard",
+    label: "Dashboard",
+    path: "/dashboard",
+    state: "app.dashboard"
+}, {
+    key: "transactions",
+    label: "Transactions",
+    path: "/transactions",
+    state: "app.transactions"
+}, {
+    key: "categories",
+    label: "Categories",
+    path: "/categories",
+    state: "app.categories"
+}, {
+    key: "settings",
+    label: "Settings",
+    path: "/settings",
+    state: "app.settings"
+}]
