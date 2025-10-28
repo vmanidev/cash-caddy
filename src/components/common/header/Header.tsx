65bd2e34ae02abba9b4f2f1b4ed890169a5a8adb
vmanidev <vmanidev@outlook.com>
2025-10-29 00:22:34 +0530

    feat(styles): add styles to navbar icon

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -9,8 +9,12 @@ function AppHeader() {
   return (
     <>
       <Sidebar openDrawer={openDrawer} setOpenDrawer={setOpenDrawer} />
-      <Grid container size={12} spacing={1} alignItems="center">
-        <Button variant="text" onClick={() => setOpenDrawer(true)}>
+      <Grid container size={12} alignItems="center">
+        <Button
+          variant="text"
+          onClick={() => setOpenDrawer(true)}
+          sx={{ justifyContent: "flex-start", padding: 0 }}
+        >
           <Menu />
         </Button>
         <Typography variant="h4" fontWeight="700" className="income-text">
