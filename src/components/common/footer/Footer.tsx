9d875ff7da11df7fcdb54cc234aa07791fbe934d
vmanidev <vmanidev@outlook.com>
2025-10-30 00:14:23 +0530

    feat(ui): add footer

---
+++ src/components/common/footer/Footer.tsx
@@ -0,0 +1,38 @@
+import { Copyright, GitHub } from "@mui/icons-material";
+import { Divider, Grid, Link, Stack } from "@mui/material";
+
+function AppFooter() {
+  return (
+    <footer>
+      <Stack
+        direction="row"
+        alignItems="center"
+        justifyContent="center"
+        divider={<Divider orientation="vertical" flexItem />}
+        spacing={2}
+      >
+        <Grid display="flex" alignItems="center">
+          <Copyright fontSize="small" />
+          <span>{new Date().getFullYear()}</span>
+        </Grid>
+        <span>Cash Caddy</span>
+        <Link
+          href="https://github.com/vmanidev/cash-caddy/blob/c16ad3aa276ab3f25fd16f6ec058ef723c35be92/LICENSE"
+          rel="noopener"
+          target="_blank"
+        >
+          MIT License
+        </Link>
+        <Link
+          href="https://github.com/vmanidev/cash-caddy"
+          rel="noopener"
+          target="_blank"
+        >
+          <GitHub />
+        </Link>
+      </Stack>
+    </footer>
+  );
+}
+
+export default AppFooter;
