d995626a420f51a004388dd26ffeafac06f02726
vmanidev <vmanidev@outlook.com>
2025-11-04 21:37:03 +0530

    fix(ui): navbar menu button size

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,5 +1,5 @@
 import { Menu } from "@mui/icons-material";
-import { Button, Grid, Typography } from "@mui/material";
+import { IconButton, Grid, Typography } from "@mui/material";
 import { useState } from "react";
 import Sidebar from "./Sidebar";
 
@@ -9,15 +9,14 @@ function AppHeader() {
   return (
     <header>
       <Sidebar openDrawer={openDrawer} setOpenDrawer={setOpenDrawer} />
-      <Grid container size={12} alignItems="center">
-        <Button
-          variant="text"
+      <Grid container size={12} spacing={2} alignItems="center">
+        <IconButton
           onClick={() => setOpenDrawer(true)}
           sx={{ justifyContent: "flex-start", padding: 0 }}
           color="inherit"
         >
-          <Menu />
-        </Button>
+          <Menu fontSize="large" />
+        </IconButton>
         <Typography variant="h4" fontWeight="700" className="income-text">
           CASH CADDY
         </Typography>
