43d52b5db867bf81d9fcd8b64517ae786f38ab60
vmanidev <vmanidev@outlook.com>
2025-11-20 22:18:18 +0530

    feat(pwa): implement PWA support for the app

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["images/favicon.png"],
+      manifest: {
+        name: "Cash Caddy",
+        short_name: "CashCaddy",
+        description: "A personal finance app for tracking expenses, setting budgets, and achieving financial goals.",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#009688",
+        icons: [
+          {
+            src: "images/logo192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "image/logo512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ],
 })
