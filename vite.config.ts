80ce1ff241a5f7c69b3b28fcb07ec5f8ae8aa740
vmanidev <vmanidev@outlook.com>
2025-11-20 23:11:31 +0530

    feat(pwa): include PWA screenshot link in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -32,6 +32,22 @@ export default defineConfig({
             sizes: "512x512",
             type: "image/png"
           }
+        ],
+        screenshots: [
+          {
+            src: "/screenshots/app-desktop.png",
+            sizes: "1280x720",
+            type: "image/png",
+            form_factor: "wide",
+            label: "Richer PWA dashboard on desktop"
+          },
+          {
+            src: "/screenshots/app-mobile.png",
+            sizes: "414x896",
+            type: "image/png",
+            form_factor: "narrow",
+            label: "Richer PWA dashboard on mobile"
+          }
         ]
       }
     })
